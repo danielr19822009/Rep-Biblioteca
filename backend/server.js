c77c2e549e6a52f89e132844390daa1eabff2e7b
daniel <danielr19822009@gmail.com>
2024-11-13 18:03:06 -0500

    cambio en nombre del llamado de la bd

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const db = mysql.createConnection({
     host: 'db-biblioteca.cdwk8g26ozev.us-east-1.rds.amazonaws.com',  //'localhost',
     user: 'admin',
     password: 'bdbiblioteca123*',
-    database: '',
+    database: 'bd_librarysm',
 });
 
 console.log("Db inicializado");
